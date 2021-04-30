74559654899ea22efc345aa8289ab08b68e8fbf1
DESKTOP-FN6F8CG\AlejoG <andresf9008@gmail.com>
2021-04-30 07:57:04 -0500

    se añadio RegisterDTO

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -2,8 +2,10 @@ using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
 using API.Data;
+using API.DTOs;
 using API.Entities;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace API.Controllers
 {
@@ -16,21 +18,31 @@ namespace API.Controllers
         } 
 
         [HttpPost("register")]
-        public async Task<ActionResult<AppUser>> Register(string username, string password)
+        public async Task<ActionResult<AppUser>> Register(RegisterDTO registerDTO)
         {
+            if (await UserExists(registerDTO.username))return BadRequest("Username ya existe");
+
             using var hmac = new HMACSHA512();
 
             var user = new AppUser
             {
-                UserName = username,
-                PasswordHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(password)),
+                UserName = registerDTO.username.ToLower(),
+                PasswordHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(registerDTO.Password)),
                 PasswordSalt = hmac.Key
+                
             };
 
             _context.Users.Add(user);
             await _context.SaveChangesAsync();
 
             return user;
+
+        }
+
+        private async Task<bool> UserExists(string username)
+        {
+            return await _context.Users.AnyAsync(variable=>variable.UserName == username.ToLower());
+
 
         }
     }
